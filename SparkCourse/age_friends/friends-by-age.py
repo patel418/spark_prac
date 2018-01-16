6f9c1f91b5f4c7b98783239fcc8d53e65e55f919
SimronP <patel418@purdue.edu>
2018-01-15 19:12:40 -0500

    more spark examples

--- SparkCourse/age_friends/friends-by-age.py
+++ SparkCourse/age_friends/friends-by-age.py
@@ -9,10 +9,12 @@ def parseLine(line):
     numFriends = int(fields[3])
     return (age, numFriends)
 
-lines = sc.textFile("file:///SparkCourse/fakefriends.csv")
+lines = sc.textFile("fakefriends.csv")
 rdd = lines.map(parseLine)
+#(x,1) the 1 will keep adding up in reduceByKey so totalsByAge shows all added up friends for age and number of people with that age
 totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
 averagesByAge = totalsByAge.mapValues(lambda x: x[0] / x[1])
 results = averagesByAge.collect()
+#results = totalsByAge.collect()
 for result in results:
     print(result)
