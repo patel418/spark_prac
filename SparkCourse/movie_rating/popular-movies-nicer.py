c67b88d5884c61986c8bbc0236643abd5ed559a9
SimronP <patel418@purdue.edu>
2018-01-17 23:02:38 -0500

    done w/ level 2

---
+++ SparkCourse/movie_rating/popular-movies-nicer.py
@@ -0,0 +1,30 @@
+from pyspark import SparkConf, SparkContext
+
+def loadMovieNames():
+    #movieNames is a dictionary, it is just like hash table movieNames[key] = value
+    movieNames = {}
+    with open("ml-100k/u.ITEM") as f:
+        for line in f:
+            fields = line.split('|')
+            movieNames[int(fields[0])] = fields[1]
+    return movieNames
+
+conf = SparkConf().setMaster("local").setAppName("PopularMovies")
+sc = SparkContext(conf = conf)
+
+#nameDict is a broadcast variable that only needs to transfer once to executor nodes. Need this if data needs to be constantly referenced
+nameDict = sc.broadcast(loadMovieNames())
+
+lines = sc.textFile("ml-100k/u.data")
+movies = lines.map(lambda x: (int(x.split()[1]), 1))
+movieCounts = movies.reduceByKey(lambda x, y: x + y)
+
+flipped = movieCounts.map( lambda x : (x[1], x[0]))
+sortedMovies = flipped.sortByKey()
+
+sortedMoviesWithNames = sortedMovies.map(lambda countMovie : (nameDict.value[countMovie[1]], countMovie[0]))
+
+results = sortedMoviesWithNames.collect()
+
+for result in results:
+    print (result)
--- SparkCourse/movie_rating/popular-movies.py
+++ SparkCourse/movie_rating/popular-movies.py
@@ -3,11 +3,11 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster("local").setAppName("PopularMovies")
 sc = SparkContext(conf = conf)
 
-lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
+lines = sc.textFile("ml-100k/u.data")
 movies = lines.map(lambda x: (int(x.split()[1]), 1))
 movieCounts = movies.reduceByKey(lambda x, y: x + y)
 
-flipped = movieCounts.map( lambda xy: (xy[1],xy[0]) )
+flipped = movieCounts.map( lambda (x, y): (y, x) )
 sortedMovies = flipped.sortByKey()
 
 results = sortedMovies.collect()
